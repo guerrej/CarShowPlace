9c73fbc89709c97a5c6263d2f0f22befc688227d
Juan Guerrero <broncos10499@yahoo.com>
2023-03-28 14:42:10 -0700

    Initial Commit

---
+++ CarShowPlace/CarShowPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarShowPlaceApp.swift
+//  CarShowPlace
+//
+//  Created by Juan Guerrero on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CarShowPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
